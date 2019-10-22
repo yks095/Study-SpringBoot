dd622740d8f63ddbaee06ddf09b93deb5b671f8c
yks095 <rltjr219@naver.com>
2019-10-22 22:20:49 +0900

    10/22 Web Server Servlet Container Customizing
     - port 변경
     - 웹 서버 사용하지 않기
     - 다른 서블릿 컨테이너로 변경

--- Spring-Boot-Getting-Started/src/main/java/kiseok/Application.java
+++ Spring-Boot-Getting-Started/src/main/java/kiseok/Application.java
@@ -1,27 +1,11 @@
 package kiseok;
 
-import me.kiseok.Holoman;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-//        SpringApplication.run(Application.class, args);
-        SpringApplication springApplication = new SpringApplication(Application.class);
-        springApplication.setWebApplicationType(WebApplicationType.NONE);
-        springApplication.run(args);
+        SpringApplication.run(Application.class, args);
     }
-
-//    @Bean
-//    public Holoman holoman() {
-//        Holoman holoman = new Holoman();
-//        holoman.setName("rltjr");
-//        holoman.setHowLong(60);
-//
-//        return holoman;
-//    }
-
 }
---
+++ Spring-Boot-Getting-Started/src/main/java/kiseok/PortListener.java
@@ -0,0 +1,16 @@
+package kiseok;
+
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component  // web server가 생성 되면 eventListener(callback)가 호출 됨
+public class PortListener implements ApplicationListener<ServletWebServerInitializedEvent> {
+
+    @Override
+    public void onApplicationEvent(ServletWebServerInitializedEvent servletWebServerInitializedEvent) {
+        ServletWebServerApplicationContext applicationContext = servletWebServerInitializedEvent.getApplicationContext();
+        System.out.println(applicationContext.getWebServer().getPort());
+    }
+}
