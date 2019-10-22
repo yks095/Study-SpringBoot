5e27bfd9aa2ac3e3d2871679a87a5228a187fd64
yks095 <rltjr219@naver.com>
2019-10-22 12:12:25 +0900

    10/22 springboot 의존성, 자동 설정 관련 내용
     - `@SpringBootApplication`
       - `@SpringBootConfiguration`
       - `@ComponentScan`
       - `@EnableAutoConfiguration`
    
     - `@ConditionalOnMissingBean`

---
+++ Spring-Boot-Getting-Started/src/main/java/kiseok/HolomanRunner.java
@@ -0,0 +1,19 @@
+package kiseok;
+
+import me.kiseok.Holoman;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HolomanRunner implements ApplicationRunner {
+
+    @Autowired
+    Holoman holoman;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(holoman);
+    }
+}
