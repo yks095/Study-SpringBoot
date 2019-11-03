5c81ab2de1927e1bfaa5c11ef0a725a682455518
yks095 <rltjr219@naver.com>
2019-11-03 21:17:37 +0900

    11/03 Type-Safe Property @ConfigurationProperties
     - 같은 키로 시작하는 여러 외부설정들을 묶어서 하나의 Bean으로 등록
     - Bean으로 등록해서 다른 Bean에 주입
       - @EnableConfigurationProperties
       - @Component
       - @Bean
     - 웅통성 있는 바인딩
       - 케밥 / 언드스코어 / 캐멀 / 대문자
     - 프로퍼티 타입 컨버젼
       - @DurationUnit
     - 프로퍼티 값 검증
       - @Validated
       - JSR-303

---
+++ Spring-Init/src/main/java/kiseok/KiseokProperties.java
@@ -0,0 +1,42 @@
+package kiseok;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
+
+@Component
+@ConfigurationProperties("kiseok")
+@Validated
+public class KiseokProperties {
+
+    @NotEmpty
+    private String name;
+    private int age;
+    private String fullName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
--- Spring-Init/src/main/java/kiseok/SampleRunner.java
+++ Spring-Init/src/main/java/kiseok/SampleRunner.java
@@ -1,6 +1,6 @@
 package kiseok;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -8,17 +8,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class SampleRunner implements ApplicationRunner {
 
-    @Value("${kiseok.fullName}")
-    private String name;
-
-    @Value("${kiseok.age}")
-    private int age;
+    @Autowired
+    KiseokProperties kiseokProperties;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("==============");
-        System.out.println(name);
-        System.out.println(age);
+        System.out.println(kiseokProperties.getName());
+        System.out.println(kiseokProperties.getAge());
         System.out.println("==============");
 
     }
