400be6be07d42b05d95ac75127da3ca2f1a4f512
yks095 <rltjr219@naver.com>
2019-11-13 16:03:07 +0900

    11/13 CORS
     - SOP를 우회하기 위한 표준 기술
     - 서로 다른 Origin끼리 Resource를 공유하는 방법을 제공
    
     - Origin?
       - URI 스키마(http, https)
       - hostname(kiseok, localhost)
       - 포트 (8080, 18080)

---
+++ Spring-CORS-Server/src/main/java/kiseok/springcorsserver/SpringcorsserverApplication.java
@@ -0,0 +1,23 @@
+package kiseok.springcorsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringcorsserverApplication {
+
+    @CrossOrigin(origins = "http://localhost:18080")
+    @GetMapping("/hello")
+    public String hello()   {
+        return "Hello";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcorsserverApplication.class, args);
+    }
+
+}
---
+++ Spring-CORS-Server/src/test/java/kiseok/springcorsserver/SpringcorsserverApplicationTests.java
@@ -0,0 +1,13 @@
+package kiseok.springcorsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcorsserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
