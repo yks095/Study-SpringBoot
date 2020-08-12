cfe46aa00928021c85b3cb7970f09f74006b7def
kiseok, Yang <rltjr219@gmail.com>
2020-08-12 17:48:13 +0900

    Database 초기화
      - JPA를 사용한 DB초기화
      - SQL script를 사용한 DB초기화

--- SpringBoot-JPA/src/main/java/com/kiseok/springbootjpa/account/Account.java
+++ SpringBoot-JPA/src/main/java/com/kiseok/springbootjpa/account/Account.java
@@ -15,6 +15,8 @@ public class Account {
 
     private String password;
 
+    private String email;
+
     public Long getId() {
         return id;
     }
@@ -37,6 +39,14 @@ public class Account {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override
