b4c147a3bbbbd09b585bb03f054b21b6e100e51e
kiseok, Yang <rltjr219@gmail.com>
2020-08-12 17:07:23 +0900

    Spring Data Jpa
      - ORM, Hibernate, JDBC, JPA, Spring Data Jpa의 연관성
      - 운영용 DB인 PostgreSQL과 테스트용 임베디드 디비 H2 설정

---
+++ SpringBoot-JPA/src/test/java/com/kiseok/springbootjpa/account/AccountRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.kiseok.springbootjpa.account;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class AccountRepositoryTest {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    public void di() {
+        Account account = new Account();
+        account.setUsername("kiseok");
+        account.setPassword("pass");
+
+        Account newAccount = accountRepository.save(account);
+
+        assertThat(newAccount).isNotNull();
+
+        Account existingAccount = accountRepository.findByUsername(newAccount.getUsername());
+        assertThat(existingAccount).isNotNull();
+
+        Account nonExistingAccount = accountRepository.findByUsername("yang");
+        assertThat(nonExistingAccount).isNull();
+    }
+}
