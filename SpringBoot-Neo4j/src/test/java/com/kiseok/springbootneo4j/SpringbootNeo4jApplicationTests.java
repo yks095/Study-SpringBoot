c48b224b0b76c0782889422824d50c4e768531c9
kiseok, Yang <rltjr219@gmail.com>
2020-08-14 15:46:26 +0900

    Study Spring Boot Neo4j 2

---
+++ SpringBoot-Neo4j/src/test/java/com/kiseok/springbootneo4j/SpringbootNeo4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiseok.springbootneo4j;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootNeo4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
