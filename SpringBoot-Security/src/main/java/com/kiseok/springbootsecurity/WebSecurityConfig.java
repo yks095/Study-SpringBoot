96761efbfaf2b2cee12082d0dba00c346e2ab80b
kiseok, Yang <rltjr219@gmail.com>
2020-08-20 15:45:40 +0900

    Spring Security에서 자동 설정 되어 있는 기능들

---
+++ SpringBoot-Security/src/main/java/com/kiseok/springbootsecurity/WebSecurityConfig.java
@@ -0,0 +1,8 @@
+package com.kiseok.springbootsecurity;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+}
---
+++ SpringBoot-Security/src/test/java/com/kiseok/springbootsecurity/SpringbootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiseok.springbootsecurity;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
